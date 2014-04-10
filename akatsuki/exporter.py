997ef395207d4e073e44e1deeb5ec79edf71808c
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-04-10 18:38:35 +0900

    Update html exporter

--- akatsuki/exporter.py
+++ akatsuki/exporter.py
@@ -24,8 +24,29 @@ def export_html(filepath, entries):
 
     for entry in entries:
         html.write('<p>\n')
-        html.write(entry['title'])
+        html.write(_entry_html(entry))
         html.write('</p>\n')
 
     # Write HTML footer lines
     html.write(HTML_FOOTER)
+
+
+def _entry_html(entry):
+    result = ''
+    result += '%s<br>\n' % entry['author']
+    result += '%s<br>\n' % entry['title']
+    if 'journal' in entry:
+        result += entry['journal'] + ' '
+        if 'year' in entry:
+            result += entry['year']
+        if 'month' in entry:
+            result += ' ' + entry['month']
+        result += ';'
+        if 'volume' in entry:
+            result += entry['volume']
+        if 'number' in entry:
+            result += '(%s)' % entry['number']
+        if 'pages' in entry:
+            result += ':%s' % entry['pages']
+        result += '.<br>\n'
+    return result

879edb6c77d717ef539674b1169e6346b15cead2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-04-15 09:43:13 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -26,6 +26,10 @@ setup(name='akatsuki',
           'Operating System :: Unix',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: PyPy'
           'Topic :: Utilities'
       ])
